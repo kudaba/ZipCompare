d913b276e04b659efd432f9b808bff6122e91642
Christian Savoie <christian.savoie@ubisoft.com>
2017-06-07 15:41:15 -0400

    Bit of cleanup on zip code test class to move bulk allocation/deallocation into setup/teardown. Added LZO, miniLZO and zlip libraries. Update readme a bit to list the libraries used in the test.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ZipCompare
-Use Code Compare to compare different compression algorithms
+Use [Code Compare](https://github.com/kudaba/CodeCompare) to compare different compression algorithms
+
+# Compression libraries used:
+
+*[lz4 (1.7.5+)](https://github.com/lz4/lz4) - Normal and fast mode
+*[Snappy (1.1.4+)](https://github.com/google/snappy)
+*[LZO / miniLZO (2.10)](http://www.oberhumer.com/opensource/lzo/)
+*[zlib (1.2.11)](https://zlib.net)
--- main.cpp
+++ main.cpp
@@ -46,29 +46,51 @@ protected:
         : CodeTest(name)
     {
         SetPass("compression", [this](Parameter const* param) { return Compress(((FileParameter const*)param)->Data()); });
-        SetPass("decompression", [this](Parameter const* param) { return Decompress(((FileParameter const*)param)->Data()); });
+        SetPassSetup("compression", [this](Parameter const* param) { CompressSetup(((FileParameter const*)param)->Data()); });
+        SetPassTeardown("compression", [this](Parameter const* param) { CompressTeardown(((FileParameter const*)param)->Data()); });
 
+        SetPass("decompression", [this](Parameter const* param) { return Decompress(((FileParameter const*)param)->Data()); });
         SetPassSetup("decompression", [this](Parameter const* param) { DecompressSetup(((FileParameter const*)param)->Data()); });
         SetPassTeardown("decompression", [this](Parameter const* param) { DecompressTeardown(((FileParameter const*)param)->Data()); });
     }
 
-    virtual vector<char> DoCompress(vector<char> const& sourceData) const = 0;
-    virtual vector<char> DoDecompress(vector<char> const& sourceData, size_t originalSize) const = 0;
+    virtual size_t CompressionSize(size_t sourceSize) const = 0;
+    virtual void DoCompress(vector<char> const& sourceData, vector<char>& destData) const = 0;
+    virtual void DoDecompress(vector<char> const& sourceData, vector<char>& destData) const = 0;
+
+    virtual void Setup(bool /*compress*/) {}
+    virtual void Teardown(bool /*compress*/) {}
 private:
     size_t Compress(vector<char> const& sourceData)
     {
-        vector<char> cdata = DoCompress(sourceData);
-        return cdata.size();
+        DoCompress(sourceData, CompressedData);
+        return CompressedData.size();
     }
+
+    void CompressSetup(vector<char> const& sourceData)
+    {
+        Setup(true);
+        CompressedData.resize(CompressionSize(sourceData.size()));
+    }
+
+    void CompressTeardown(vector<char> const& sourceData)
+    {
+        CompressedData = vector<char>();
+        Teardown(true);
+    }
+
     size_t Decompress(vector<char> const& sourceData)
     {
-        UnCompressedData = DoDecompress(CompressedData, sourceData.size());
+        DoDecompress(CompressedData, UnCompressedData);
         return UnCompressedData.size();
     }
 
     void DecompressSetup(vector<char> const& sourceData)
     {
-        CompressedData = DoCompress(sourceData);
+        Setup(false);
+        CompressSetup(sourceData);
+        DoCompress(sourceData, CompressedData);
+        UnCompressedData.resize(sourceData.size());
     }
 
     void DecompressTeardown(vector<char> const& sourceData)
@@ -76,8 +98,9 @@ private:
         assert(sourceData.size() == UnCompressedData.size());
         assert(memcmp(sourceData.data(), UnCompressedData.data(), UnCompressedData.size()) == 0);
 
-        CompressedData = vector<char>();
         UnCompressedData = vector<char>();
+        CompressTeardown(sourceData);
+        Teardown(false);
     }
 
     vector<char> CompressedData;
@@ -92,26 +115,21 @@ public:
     LZ4Test() : CompressionTest("lz4") {}
 
 protected:
-    vector<char> DoCompress(vector<char> const& sourceData) const override
+    size_t CompressionSize(size_t sourceSize) const override
     {
-        vector<char> data;
-        data.resize(LZ4_compressBound(sourceData.size()));
-
-        int result = LZ4_compress_default(sourceData.data(), data.data(), sourceData.size(), data.size());
+        return LZ4_compressBound(sourceSize);
+    }
+    void DoCompress(vector<char> const& sourceData, vector<char>& destData) const override
+    {
+        int result = LZ4_compress_default(sourceData.data(), destData.data(), sourceData.size(), destData.size());
         assert(result >= 0);
-        data.resize(result);
-        return data;
+        destData.resize(result);
     }
 
-    vector<char> DoDecompress(vector<char> const& sourceData, size_t originalSize) const override
+    void DoDecompress(vector<char> const& sourceData, vector<char>& destData) const override
     {
-        vector<char> data;
-        data.resize(originalSize);
-
-        int result = LZ4_decompress_safe(sourceData.data(), data.data(), sourceData.size(), data.size());
-        assert(result >= 0);
-        data.resize(result);
-        return data;
+        int result = LZ4_decompress_safe(sourceData.data(), destData.data(), sourceData.size(), destData.size());
+        assert(result >= 0 && result == destData.size());
     }
 };
 
@@ -121,25 +139,21 @@ public:
     LZ4FastTest() : CompressionTest("lz4Fast") {}
 
 protected:
-    vector<char> DoCompress(vector<char> const& sourceData) const override
+    size_t CompressionSize(size_t sourceSize) const override
     {
-        vector<char> data;
-        data.resize(LZ4_compressBound(sourceData.size()));
-
-        int result = LZ4_compress_fast(sourceData.data(), data.data(), sourceData.size(), data.size(), 10);
+        return LZ4_compressBound(sourceSize);
+    }
+    void DoCompress(vector<char> const& sourceData, vector<char>& destData) const override
+    {
+        int result = LZ4_compress_fast(sourceData.data(), destData.data(), sourceData.size(), destData.size(), 10);
         assert(result >= 0);
-        data.resize(result);
-        return data;
+        destData.resize(result);
     }
 
-    vector<char> DoDecompress(vector<char> const& sourceData, size_t originalSize) const override
+    void DoDecompress(vector<char> const& sourceData, vector<char>& destData) const override
     {
-        vector<char> data;
-        data.resize(originalSize);
-
-        int result = LZ4_decompress_fast(sourceData.data(), data.data(), data.size());
-        assert(result >= 0);
-        return data;
+        int result = LZ4_decompress_fast(sourceData.data(), destData.data(), destData.size());
+        assert(result >= 0 && result == sourceData.size());
     }
 };
 
@@ -151,31 +165,161 @@ public:
     SnappyTest() : CompressionTest("Snappy") {}
 
 protected:
-    vector<char> DoCompress(vector<char> const& sourceData) const override
+    size_t CompressionSize(size_t sourceSize) const override
     {
-        vector<char> data;
-        data.resize(snappy_max_compressed_length(sourceData.size()));
-
-        size_t result = data.size();
-        snappy_status status = snappy_compress(sourceData.data(), sourceData.size(), data.data(), &result);
+        return snappy_max_compressed_length(sourceSize);
+    }
+    void DoCompress(vector<char> const& sourceData, vector<char>& destData) const override
+    {
+        size_t result = destData.size();
+        snappy_status status = snappy_compress(sourceData.data(), sourceData.size(), destData.data(), &result);
         assert(status == SNAPPY_OK && result >= 0);
-        data.resize(result);
-        return data;
+        destData.resize(result);
     }
 
-    vector<char> DoDecompress(vector<char> const& sourceData, size_t originalSize) const override
+    void DoDecompress(vector<char> const& sourceData, vector<char>& destData) const override
     {
-        vector<char> data;
-        data.resize(originalSize);
-
-        size_t result = originalSize;
-        snappy_status status = snappy_uncompress(sourceData.data(), sourceData.size(), data.data(), &result);
-        assert(status == SNAPPY_OK && result == originalSize);
-        return data;
+        size_t result = destData.size();
+        snappy_status status = snappy_uncompress(sourceData.data(), sourceData.size(), destData.data(), &result);
+        assert(status == SNAPPY_OK && result == destData.size());
     }
 };
 
-class ExampleHarness : public TestHarness
+#include "zlib/zlib.h"
+
+class ZLibTest : public CompressionTest
+{
+public:
+    ZLibTest() : CompressionTest("zlib") {}
+
+protected:
+    static voidpf alloc(voidpf opaque, uInt items, uInt size)
+    {
+        return new char[items*size];
+    }
+    static void free(voidpf opaque, voidpf address)
+    {
+        delete [] (char*)address;
+    }
+
+    size_t CompressionSize(size_t sourceSize) const override
+    {
+        return compressBound(sourceSize);
+    }
+    void DoCompress(vector<char> const& sourceData, vector<char>& destData) const override
+    {
+        // don't init/end in setup/teardown to simulate use of 'compress' method
+        z_stream strm;
+        strm.zalloc = &alloc;
+        strm.zfree = &free;
+        strm.opaque = Z_NULL;
+
+        deflateInit(&strm, Z_DEFAULT_COMPRESSION);
+        strm.avail_in = sourceData.size();
+        strm.next_in = (Bytef*)sourceData.data();
+
+        strm.avail_out = destData.size();
+        strm.next_out = (Bytef*)destData.data();
+
+        int status = deflate(&strm, Z_FINISH);
+        assert(status == Z_STREAM_END && strm.total_out >= 0);
+        destData.resize(strm.total_out);
+        deflateEnd(&strm);
+    }
+
+    void DoDecompress(vector<char> const& sourceData, vector<char>& destData) const override
+    {
+        // don't init/end in setup/teardown to simulate use of 'decompress' method
+        z_stream strm;
+        strm.zalloc = &alloc;
+        strm.zfree = &free;
+        strm.opaque = Z_NULL;
+
+        inflateInit(&strm);
+        strm.avail_in = sourceData.size();
+        strm.next_in = (Bytef*)sourceData.data();
+
+        strm.avail_out = destData.size();
+        strm.next_out = (Bytef*)destData.data();
+
+        int status = inflate(&strm, Z_FINISH);
+        assert(status == Z_STREAM_END && strm.total_out == destData.size());
+        inflateEnd(&strm);
+    }
+};
+
+#include "minilzo/minilzo.h"
+
+class MiniLZOTest : public CompressionTest
+{
+public:
+    MiniLZOTest() : CompressionTest("miniLZO") {}
+
+protected:
+    size_t CompressionSize(size_t sourceSize) const override
+    {
+        // taken from testmini.c
+        return sourceSize + sourceSize / 16 + 64 + 3;
+    }
+    void DoCompress(vector<char> const& sourceData, vector<char>& destData) const override
+    {
+        lzo_align_t workMemory[(LZO1X_1_MEM_COMPRESS + sizeof(lzo_align_t) - 1) / sizeof(lzo_align_t)];
+
+        lzo_uint result = destData.size();
+        int status = minilzo1x_1_compress((unsigned char const*)sourceData.data(), sourceData.size(), (unsigned char*)destData.data(), &result, &workMemory);
+        assert(status == LZO_E_OK && result >= 0);
+        destData.resize(result);
+    }
+
+    void DoDecompress(vector<char> const& sourceData, vector<char>& destData) const override
+    {
+        lzo_uint result = destData.size();
+        int status = minilzo1x_decompress((unsigned char const*)sourceData.data(), sourceData.size(), (unsigned char*)destData.data(), &result, NULL);
+        assert(status == LZO_E_OK && result == destData.size());
+    }
+};
+
+#include "lzo/lzo1c.h"
+
+class LZO1CTest : public CompressionTest
+{
+public:
+    LZO1CTest() : CompressionTest("LZO1C-3")
+    {
+        static bool init = false;
+        if (!init)
+        {
+            lzo_init();
+            init = true;
+        }
+    }
+
+protected:
+    size_t CompressionSize(size_t sourceSize) const override
+    {
+        // taken from testmini.c
+        return sourceSize + sourceSize / 16 + 64 + 3;
+    }
+    void DoCompress(vector<char> const& sourceData, vector<char>& destData) const override
+    {
+        lzo_align_t workMemory[(LZO1C_MEM_COMPRESS + sizeof(lzo_align_t) - 1) / sizeof(lzo_align_t)];
+
+        lzo_uint result = destData.size();
+        int status = lzo1c_3_compress((unsigned char const*)sourceData.data(), sourceData.size(), (unsigned char*)destData.data(), &result, &workMemory);
+        assert(status == LZO_E_OK && result >= 0);
+        destData.resize(result);
+    }
+
+    void DoDecompress(vector<char> const& sourceData, vector<char>& destData) const override
+    {
+        lzo_uint result = destData.size();
+        int status = lzo1c_decompress((unsigned char const*)sourceData.data(), sourceData.size(), (unsigned char*)destData.data(), &result, NULL);
+        assert(status == LZO_E_OK && result == destData.size());
+    }
+};
+
+
+class ZipHarness : public TestHarness
 {
     unique_ptr<TestSuite const> TestHarness::CreateTest() const override
     {
@@ -190,9 +334,13 @@ class ExampleHarness : public TestHarness
         suite->AddTest(unique_ptr<CodeTest>(new LZ4Test()));
         suite->AddTest(unique_ptr<CodeTest>(new LZ4FastTest()));
         suite->AddTest(unique_ptr<CodeTest>(new SnappyTest()));
+        suite->AddTest(unique_ptr<CodeTest>(new ZLibTest()));
+        suite->AddTest(unique_ptr<CodeTest>(new MiniLZOTest()));
+        suite->AddTest(unique_ptr<CodeTest>(new LZO1CTest()));
 
         TestConfig config;
         config.CustomResult.Sort = PassConfig::Percentage;
+        config.Performance.Logarithmic = true;
 
         suite->SetPassConfig("compression", config);
 
@@ -213,5 +361,5 @@ class ExampleHarness : public TestHarness
 
 void main()
 {
-    Bootstrap::RunTests(ExampleHarness());
+    Bootstrap::RunTests(ZipHarness());
 }
